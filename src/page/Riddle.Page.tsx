c398f57be1cec2fe29fd25f7a7d0ff0532fade9a
Moti7950 <shalom7950@gmail.com>
2025-09-02 18:22:57 +0300

    fix bugs

--- src/page/Riddle.Page.tsx
+++ src/page/Riddle.Page.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router";
 
-export default function RiddlePage(){
-    return(
-        <>
-        <h1>Hi from Riddle page</h1>
-        <Link to="/">Go to homepage</Link>
-        </>
-    )
+export default function RiddlePage() {
+  return (
+    <>
+      <h1>Hi from Riddle page</h1>
+      <h2>Riddle:</h2>
+      <p>"What is 5 + 3?"</p>
+        <label htmlFor="Answer"></label>
+        <input id="Answer"  type="text" />
+      <Link to="/">Go to homepage</Link>
+    </>
+  );
 }
